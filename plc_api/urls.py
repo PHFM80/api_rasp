c78f30f4cf30ebdbc3137405b8ee2c5f031d085f
PHFM80 <pablofloresmaza@gmail.com>
2025-05-04 12:33:06 -0300

    prueba 04/05/2025

--- plc_api/urls.py
+++ plc_api/urls.py
@@ -1,10 +1,8 @@
 print("Cargando rutas de plc_api")
 from django.urls import path
-from .views import leer_dato_s7_view, leer_dato_modbus_view,leer_analogico_ai1
-
+from .views import leer_analogico_ai1, leer_estado_lampara
 urlpatterns = [
-    path('leer_s7/', leer_dato_s7_view, name='leer-s7'),
-    path('leer_modbus/', leer_dato_modbus_view, name='leer-modbus'),
     path('leer_ai1/', leer_analogico_ai1, name='leer_ai1'),
+    path('api/lamp/', leer_estado_lampara, name='leer_estado_lampara'),
 ]
 
--- plc_api/views.py
+++ plc_api/views.py
@@ -1,58 +1,12 @@
 # plc_api/views.py
-# plc_api/views.py
+
 from django.http import JsonResponse
 import snap7
 from snap7.util import get_int
 from snap7 import type as types
 from snap7 import client
-from pymodbus.client import ModbusTcpClient
+from snap7.util import get_bool
 from snap7.util import get_int
-
-
-def leer_dato_s7_view(request):
-    try:
-        plc_ip = '192.168.0.3'
-        rack = 0
-        slot = 0  # para LOGO! 8 suele ser 0
-
-        client = snap7.client.Client()
-        client.connect(plc_ip, rack, slot)
-
-        # Leer 2 bytes de memoria (por ejemplo, a partir de MB0)
-        data = client.read_area(snap7.types.Areas.MK, 0, 0, 2)
-        valor = snap7.util.get_int(data, 0)
-
-        client.disconnect()
-
-        return JsonResponse({'valor_s7': valor})
-
-    except Exception as e:
-        return JsonResponse({'error': str(e)})
-
-
-
-def leer_dato_modbus_view(request):
-    try:
-        plc_ip = '192.168.0.3'
-        port = 502  # o 510 si usás ese
-
-        client = ModbusTcpClient(plc_ip, port=port)
-        client.connect()
-
-        # Leer holding register 0 (ajustá según config del LOGO!)
-        rr = client.read_holding_registers(address=0, count=2, unit=1)
-        client.close()
-
-        if rr.isError():
-            return JsonResponse({'error': 'Error en lectura Modbus'})
-
-        valor = rr.registers[0]
-        return JsonResponse({'valor_modbus': valor})
-
-    except Exception as e:
-        return JsonResponse({'error': str(e)})
-
-
 
 def leer_analogico_ai1(request):
     try:
@@ -69,5 +23,29 @@ def leer_analogico_ai1(request):
         plc.disconnect()
 
         return JsonResponse({'valor_ai1': valor})
+    except Exception as e:
+        return JsonResponse({'error': str(e)})
+    
+
+
+def leer_estado_lampara(request):
+    try:
+        plc_ip = '192.168.0.3'
+        rack = 0
+        slot = 0  # LOGO! 8 usualmente usa slot 0
+
+        plc = client.Client()
+        plc.connect(plc_ip, rack, slot)
+
+        # Leer 1 byte desde dirección 0 de la memoria (VM0)
+        data = plc.read_area(types.Areas.MK, 0, 0, 1)
+
+        # Extraer el bit 0 (VB0.0)
+        estado = get_bool(data, 0, 0)
+
+        plc.disconnect()
+
+        return JsonResponse({'lampara': 'ON' if estado else 'OFF'})
+
     except Exception as e:
         return JsonResponse({'error': str(e)})
